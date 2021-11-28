2d1d56175d5628250521ca0f3258f1d8b54a5f9f
Swapfn <abdelrahmanalix@gmail.com>
2021-11-28 20:20:09 +0200

    Insertsort#1

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -15,6 +15,28 @@
 
 function insertionSort(nums) {
   // code goes here
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] > nums[i+1]){
+      const low = nums[i+1];
+      nums[i+1] = nums[i];
+      nums[i] = low;
+      if (i === 0 || (nums[i+1] > nums[i] && nums[i] > nums[i-1])){
+        continue;
+      }else{
+      for(let j=i; j>0; j--)
+      {
+      if (nums[j-1]>nums[j]){
+      const low = nums[j];
+      nums[j] = nums[j-1];
+      nums[j-1] = low;
+      }else{
+        break;
+          }
+      }
+      }
+      }
+    }
+  return nums;
 }
 
 // unit tests
