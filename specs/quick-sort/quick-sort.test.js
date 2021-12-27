20e793a062cff9e2ff8c1a9322981101b7bbc081
Swapfn <abdelrahmanalix@gmail.com>
2021-12-27 17:15:58 +0200

    quicksort

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -14,13 +14,40 @@
 
 function quickSort(nums) {
   // code goes here
+  // base case
+  if (nums.length <= 1){
+    return nums;
+  }
+  // create pivot
+  const pivot = nums[nums.length - 1];
+  // seperate left and right arrays
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < pivot) {
+      left.push(nums[i]);
+    } else {
+      right.push(nums[i]);
+    }
+  }
+  // call quicksort on left and right arrays
+  const sortedLeft = quickSort(left);
+  const sortedRight = quickSort(right);
+  
+  return sortedLeft.concat(pivot, sortedRight);
 }
 
+
+
+const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
+const answer = quickSort(input);
+console.log(answer);
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+/* test.skip("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
   expect(answer).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-});
+}); */
